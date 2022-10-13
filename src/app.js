b17cdc7fa53be185632b51643b3eb4131a947afb
jsaique <jsaique11@gmail.com>
2022-10-13 13:16:24 -0400

    Intial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const hamburgerButton = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
+
+const toggleButton = function () {
+  navList.classList.toggle('show');
+};
+
+hamburgerButton.addEventListener('click', toggleButton);
