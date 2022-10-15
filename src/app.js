47c7ad5001c87c3e1ebfaf7b69b1e5dce2d7b6a8
jsaique <jsaique11@gmail.com>
2022-10-15 15:42:30 -0400

    added message me button

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const contactMe = document.querySelector('.contact-me-modal');
+console.log(contactMe);
+
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('nav-list');
 
@@ -5,4 +13,22 @@ const toggleButton = function () {
   navList.classList.toggle('show');
 };
 
+// Event Listeners
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 hamburgerButton.addEventListener('click', toggleButton);
+
+contactMe.addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
