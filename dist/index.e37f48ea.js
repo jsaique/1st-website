3f526d95e0291ae26a9e00710c1fb1be57d88337
jsaique <jsaique11@gmail.com>
2022-10-20 22:49:21 -0400

    added email form style and smooth scrolling

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -533,13 +533,18 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"aenu9":[function(require,module,exports) {
 "use strict";
-if (module.hot) module.hot.accept();
+// if (module.hot) {
+//   module.hot.accept();
+// }
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const contactMe = document.querySelector(".contact-me-modal");
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
+const navLink = document.querySelector(".nav-links");
+const btnScrollTo = document.querySelector(".button");
+const aboutMe = document.querySelector("#about-me");
 const toggleButton = function() {
     navList.classList.toggle("show");
 };
@@ -552,10 +557,34 @@ const closeModal = function() {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
 };
+const smoothScroll = function(e) {
+    const s1coords = aboutMe.getBoundingClientRect();
+    window.scrollTo({
+        left: s1coords.left + window.pageXOffset,
+        top: s1coords.top + window.pageYOffset,
+        behavior: "smooth"
+    });
+};
+// Page navigation
+// Event delegation
+// 1.Add the eventlistener to a common parent element
+// 2. Determine which element originated the event
+navLink.addEventListener("click", function(e) {
+    e.preventDefault();
+    // Matching strategy
+    if (e.target.classList.contains("nav-link")) {
+        const id = e.target.getAttribute("href");
+        console.log(id);
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+});
 hamburgerButton.addEventListener("click", toggleButton);
 contactMe.addEventListener("click", showModal);
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+btnScrollTo.addEventListener("click", smoothScroll);
 
 },{}]},["fA0o9","aenu9"], "aenu9", "parcelRequirec3c7")
 
